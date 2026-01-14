98c582562111e7b61f8e5da07fa014671de59799
mbs0032 <mbs0032@alu.medac.es>
2026-01-14 19:18:28 +0100

    Añadido proyecto Yumplace_1

---
+++ Yumplace_1/Yumplace_1/app/src/main/java/controlador/RegisterActivity.java
@@ -0,0 +1,17 @@
+package controlador;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.engiri.yumplace.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+    }
+}
